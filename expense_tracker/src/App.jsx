e5c2f377e18e254f7c07ded562d1f117704c975a
imamanjangra <imamanjangra@gmail.com>
2025-10-05 17:26:28 +0530

    Add a React Project file

---
+++ expense_tracker/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+    <p className='bg-green-400'>Hello </p>
+    </>
+  )
+}
+
+export default App
