95ebd3dd81ea8a9ef18f3048687a9ee3f895a346
imamanjangra <imamanjangra@gmail.com>
2025-10-06 14:26:29 +0530

    add graph

--- expense_tracker/src/Components/Dashboard.jsx
+++ expense_tracker/src/Components/Dashboard.jsx
@@ -1,5 +1,15 @@
 import { useEffect, useState } from "react";
 import { useExpense } from "../Context/Expense_Context";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 function Dashboard() {
   const { Expense } = useExpense();
@@ -7,8 +17,6 @@ function Dashboard() {
   const [Ex, setEx] = useState(0);
 
   useEffect(() => {
-   
-
     let totalIncome = 0;
     let totalExpense = 0;
 
@@ -27,6 +35,8 @@ function Dashboard() {
   return (
     <div className="max-w-4xl mx-auto mt-10 bg-white rounded-2xl shadow-md p-8 text-center">
       <h2 className="text-3xl font-bold text-blue-600 mb-6">Dashboard</h2>
+
+      {/* === Summary Cards === */}
       <div className="grid grid-cols-2 md:grid-cols-3 gap-6">
         <div className="bg-green-100 p-6 rounded-xl shadow">
           <h3 className="text-xl font-semibold text-green-700">Total Income</h3>
@@ -43,6 +53,29 @@ function Dashboard() {
           <p className="text-2xl font-bold mt-2">{Income - Ex}</p>
         </div>
       </div>
+
+     
+      <div className="mt-10" style={{ width: "100%", height: 400 }}>
+        <ResponsiveContainer>
+          <BarChart
+          data={[
+            {name : "Income" , amount : Income},
+            {name : "Expense" , amount : Ex}
+          ]}
+             margin={{ top: 20, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray='3 3'/>
+            <XAxis data="name"/>
+            <YAxis/>
+            <Tooltip/>
+            <Legend/>
+            <Bar dataKey="amount" fill="#8884d8"/>
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
+
+
+      
     </div>
   );
 }
--- expense_tracker/src/Components/Footer.jsx
+++ expense_tracker/src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bg-gray-900 text-gray-300 w-full fixed bottom-0 left-0 z-50">
+    <footer className="bg-gray-900 text-gray-300 w-full mt-auto">
       <div className="max-w-6xl mx-auto px-4 py-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         
         {/* About Section */}
