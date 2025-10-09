ef6660953eb294fee7aabe91f4a9047062a197e1
imamanjangra <imamanjangra@gmail.com>
2025-10-09 23:02:11 +0530

    Add Wether app file

---
+++ wetherapp/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <p className='bg-green-200'>hello it just a test </p>
+    </>
+  )
+}
+
+export default App
