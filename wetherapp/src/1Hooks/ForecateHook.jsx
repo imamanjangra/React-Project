0b1cf0b9dfaf72328bcf32a77a1098a2e488806e
imamanjangra <imamanjangra@gmail.com>
2025-10-12 21:58:08 +0530

    ok

--- wetherapp/src/1Hooks/ForecateHook.jsx
+++ wetherapp/src/1Hooks/ForecateHook.jsx
@@ -5,11 +5,11 @@ import { useSelector } from "react-redux"
 function ForecasteHook(){
     const wether = useSelector(state => state.wether)
 
-    const [Forcst_Api , setForcat_Api] = useState("")
+    const [Forcst_Api , setForcat_Api] = useState({})
      const api_key = "2a591f52675ebfba308e143ffe71bbca"
 
      useEffect(() => {
-        fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${wether.text}&appid=${api_key}`)
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${wether.text}&appid=${api_key}`)
         .then(resp => resp.json())
         .then(resp => {
             if(resp.cod == "200"){
@@ -19,6 +19,9 @@ function ForecasteHook(){
                 alert("City name not found. Please enter a valid city name!")
                 
             }
+           
+        }) .catch(() => {
+            alert("Unable to fetch weather data. Please check your connection.");
         })
 
     } , [wether.text])
